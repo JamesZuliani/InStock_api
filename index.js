5411d38caf15d56ae6eba5232ce1524f38febc7c
James Zuliani <zulianijames@gmail.com>
2023-04-25 16:03:16 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { PORT } = process.env;
+
+app.use(cors());
+app.use(express.json());
+
+const warehouseRoute =  require ("./routes/warehouse.js")
+app.use("/warehouse", warehouseRoute)
+
+
+app.listen(8080, function () {
+    console.log("here is the server on port " + PORT);
+  });
