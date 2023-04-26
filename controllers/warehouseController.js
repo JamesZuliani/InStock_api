5863fcef82910d074b5ed21c9b2aed15d92ba8e3
Nima Bargestan <nimam700@gmail.com>
2023-04-26 00:58:43 -0400

    finish post endpoint

---
+++ controllers/warehouseController.js
@@ -0,0 +1,91 @@
+const { v4 } = require("uuid");
+
+// knex configuration
+const knexConfig = require("../knexfile");
+const knex = require("knex");
+const db = knex(knexConfig);
+//////////////////////////////////
+
+
+// POST
+module.exports.postWarehouse = async (req, res) => {
+  // Check if we have sufficient body data
+  if (Object.keys(req.body).length !== 8) {
+    res.status(400).json({ error: "insufficient prameter" });
+    // if we have complete body data:
+  } else {
+    // loop through the object and check if its not empty
+    for (key in req.body) {
+      if (req.body[key] === "") {
+        return res.status(400).json({ error: `${key} is empty` });
+      }
+      //   check for valid Phone number
+      else if (
+        key === "contact_phone" &&
+        req.body[key].search(/^\+\d{1,3}\ \(\d{3}\) \d{3}-\d{4}$/) !== 0
+      ) {
+        return res.status(400).json({
+          error: `${key}: < '${req.body[key]}' > has incorrect format`,
+        });
+      }
+      //   check for valid Email address
+      else if (
+        key === "contact_email" &&
+        req.body[key].search(
+          /^[a-zA-Z0-9._%+]+@[a-zA-Z0-9.]+\.[a-zA-Z]{2,}$/
+        ) !== 0
+      ) {
+        return res.status(400).json({
+          error: `${key}: < '${req.body[key]}' > has incorrect format`,
+        });
+      }
+    }
+
+    // now destructure the request
+    const {
+      warehouse_name,
+      address,
+      city,
+      country,
+      contact_name,
+      contact_position,
+      contact_phone,
+      contact_email,
+    } = req.body;
+
+    try {
+      // create a uuid for each row
+      const newId = v4();
+      // INSERT INTO warehouses
+      await db("warehouses").insert({
+        id: newId,
+        warehouse_name,
+        address,
+        city,
+        country,
+        contact_name,
+        contact_position,
+        contact_phone,
+        contact_email,
+      });
+      // Show the added row as the requested format
+      const insertedRow = await db("warehouses")
+        .select(
+          "warehouse_name",
+          "address",
+          "city",
+          "country",
+          "contact_name",
+          "contact_position",
+          "contact_phone",
+          "contact_email"
+        )
+        .where("id", newId)
+        .first();
+      res.json(insertedRow);
+    } catch (err) {
+        // prefrred to not show the error message.
+      res.status(400).json({error: "Failed to create the warehouse entry."});
+    }
+  }
+};
