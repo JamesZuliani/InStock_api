fa47f250761631e486dc24e75f035a87ed2d4014
Daniel Cao <dcao7783@gmail.com>
2023-04-26 15:01:49 -0600

    creating API for getting the single warehouse

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -4,6 +4,10 @@ const warehouseController = require("../controllers/warehouseController");
 //Back-End: API to GET all warehouses
 router.get("/", warehouseController.fetchAll);
 
+//Back-End: API to GET the single warehouses
+
+router.get("/:warehouseId", warehouseController.fetchId);
+
 // Back-End: API to POST/CREATE a New Warehouse
 router.post("/", warehouseController.postWarehouse);
 
