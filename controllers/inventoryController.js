99541ba77d90e3450a6b85647b5f79b5bf4b0189
Nima Bargestan <nimam700@gmail.com>
2023-04-26 21:23:32 -0400

    final commit

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -7,6 +7,28 @@ const db = knex(knexConfig);
 //////////////////////////////////
 
 // GET Single warehouses
-module.exports.fetchSingle = (req, res) => {
+module.exports.fetchSingle = async (req, res) => {
+  try {
+    const foundRow = await db("inventories")
+      .join("warehouses", "inventories.warehouse_id", "=", "warehouses.id")
+      .where({ "inventories.id": req.params.inventoryId })
+      .select(
+        "inventories.id",
+        "warehouse_name",
+        "item_name",
+        "description",
+        "category",
+        "status",
+        "quantity"
+      )
+      .first();
+    if (foundRow) {
+      res.json(foundRow);
+    } else {
+        res.status(404).json({error: `inventory id: {${req.params.inventoryId}} not found`});
 
-}
+    }
+  } catch (error) {
+    res.status(500).json({ error: "cannot fetch the selected inventory" });
+  }
+};
--- routes/inventory.js
+++ routes/inventory.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const inventoryController = require("../controllers/inventoryController");
 
 // Back-End: API to GET a Single Inventory Item
-router.get("/inventoryId", inventoryController.fetchSingle);
+router.get("/:inventoryId", inventoryController.fetchSingle);
 
 
 module.exports = router;
